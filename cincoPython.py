aac80ec4a81d7ef8bee05ba6274b7fa23285f4da
Camily Ghellar <ghellarcamily@gmail.com>
2023-03-17 14:43:15 -0300

    atualizações no código

--- cincoPython.py
+++ cincoPython.py
@@ -3,9 +3,24 @@ alunos= []
 #aluno 1
 nome_aluno1= input("Digite o nome do primeiro aluno: ")
 nota1_aluno1= float(input("Digite a primeira nota do primeiro aluno: "))    
+if nota1_aluno1<0 or nota1_aluno1>10:
+    print ("Digite uma nota válida (0-10)")
+    nota1_aluno1= float(input("Digite a primeira nota do primeiro aluno: ")) 
+
 nota2_aluno1= float(input("Digite a segunda nota do primeiro aluno: "))    
+if nota2_aluno1<0 or nota2_aluno1>10:
+    print ("Digite uma nota válida (0-10)")
+    nota2_aluno1= float(input("Digite a segunda nota do primeiro aluno: ")) 
+
 nota3_aluno1= float(input("Digite a terceira nota do primeiro aluno: "))    
+if nota3_aluno1<0 or nota3_aluno1>10:
+    print ("Digite uma nota válida (0-10)")
+    nota3_aluno1= float(input("Digite a terceira nota do primeiro aluno: ")) 
+
 nota4_aluno1= float(input("Digite a quarta nota do primeiro aluno: "))    
+if nota4_aluno1<0 or nota4_aluno1>10:
+    print ("Digite uma nota válida (0-10)")
+    nota4_aluno1= float(input("Digite a quarta nota do primeiro aluno: ")) 
 
 media_aluno1= (nota1_aluno1+nota2_aluno1+nota3_aluno1+nota4_aluno1)/4
 
@@ -15,9 +30,24 @@ alunos.append([nome_aluno1, media_aluno1])
 #aluno 2
 nome_aluno2= input("Digite o nome do segundo aluno: ")
 nota1_aluno2= float(input("Digite a primeira nota do segundo aluno: "))    
+if nota1_aluno2<0 or nota1_aluno2>10:
+    print ("Digite uma nota válida (0-10)")
+    nota1_aluno2= float(input("Digite a primeira nota do segundo aluno: ")) 
+
 nota2_aluno2= float(input("Digite a segunda nota do segundo aluno: "))    
+if nota2_aluno2<0 or nota2_aluno2>10:
+    print ("Digite uma nota válida (0-10)")
+    nota2_aluno2= float(input("Digite a segunda nota do segundo aluno: ")) 
+
 nota3_aluno2= float(input("Digite a terceira nota do segundo aluno: "))    
+if nota3_aluno2<0 or nota3_aluno2>10:
+    print ("Digite uma nota válida (0-10)")
+    nota3_aluno2= float(input("Digite a terceira nota do segundo aluno: ")) 
+
 nota4_aluno2= float(input("Digite a quarta nota do segundo aluno: "))    
+if nota4_aluno2<0 or nota4_aluno2>10:
+    print ("Digite uma nota válida (0-10)")
+    nota4_aluno2= float(input("Digite a quarta nota do segundo aluno: ")) 
 
 media_aluno2= (nota1_aluno2+nota2_aluno2+nota3_aluno2+nota4_aluno2)/4
 
@@ -27,9 +57,24 @@ alunos.append([nome_aluno2, media_aluno2])
 #aluno 3
 nome_aluno3= input("Digite o nome do terceiro aluno: ")
 nota1_aluno3= float(input("Digite a primeira nota do terceiro aluno: "))    
+if nota1_aluno3<0 or nota1_aluno3>10:
+    print ("Digite uma nota válida (0-10)")
+    nota1_aluno3= float(input("Digite a primeira nota do terceiro aluno: ")) 
+
 nota2_aluno3= float(input("Digite a segunda nota do terceiro aluno: "))    
+if nota2_aluno3<0 or nota2_aluno3>10:
+    print ("Digite uma nota válida (0-10)")
+    nota2_aluno3= float(input("Digite a segunda nota do terceiro aluno: ")) 
+
 nota3_aluno3= float(input("Digite a terceira nota do terceiro aluno: "))    
+if nota3_aluno3<0 or nota3_aluno3>10:
+    print ("Digite uma nota válida (0-10)")
+    nota3_aluno3= float(input("Digite a terceira nota do terceiro aluno: ")) 
+
 nota4_aluno3= float(input("Digite a quarta nota do terceiro aluno: "))    
+if nota4_aluno3<0 or nota4_aluno3>10:
+    print ("Digite uma nota válida (0-10)")
+    nota4_aluno3= float(input("Digite a quarta nota do terceiro aluno: ")) 
 
 media_aluno3= (nota1_aluno3+nota2_aluno3+nota3_aluno3+nota4_aluno3)/4
 
